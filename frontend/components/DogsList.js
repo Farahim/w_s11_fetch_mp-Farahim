89bbe1120f4e87dc377eb4bb4cbacc7ff511a2ac
Farahim Ibrahimli <i.farahim@gmail.com>
2024-11-14 00:09:37 -0500

    done

--- frontend/components/DogsList.js
+++ frontend/components/DogsList.js
@@ -1,17 +1,21 @@
 import React from 'react'
 
-export default function DogsList() {
+export default function DogsList( dogs, getDogs,setCurrentDogs ) {
   return (
     <div>
       <h2>Dogs Shelter</h2>
       <ul>
-        <li>
-          Fido, Bulldog, NOT adopted
-          <div>
-            <button>Edit</button>
-            <button>Delete</button>
-          </div>
-        </li>
+          {
+              dogs.map(dog => (
+                  <li key={dog.id}>
+                      {dog.name}, {dog.bread}, {dog.adapted? '' : 'NOT'}adopted
+                      <div>
+                          <button>Edit</button>
+                          <button>Delete</button>
+                      </div>
+                  </li>
+              ))
+          }
       </ul>
     </div>
   )
